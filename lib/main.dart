c7a4f1bd09147c60d538e929ae9b3218c664db92
Nyaacinth <80994270+Nyaacinth@users.noreply.github.com>
2024-07-09 10:33:47 +0800

    Remove Functional Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:functional_widget_annotation/functional_widget_annotation.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-
-part 'main.g.dart';
 
 void main() {
   runApp(const ProviderScope(child: MainApp()));
 }
 
-@swidget
-Widget _mainApp(BuildContext context) {
-  return const MaterialApp(
-    home: Scaffold(
-      body: Center(
-        child: Text('Hello World!'),
+class MainApp extends HookConsumerWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
